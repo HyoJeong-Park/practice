b877cd6f567732e300d31310bc3525ecd0a93e1d
HyoJeong-Park <parkhj3003@gmail.com>
2023-11-05 21:56:37 +0900

    Feat: useMediaQuery 실제로 사용해보기

--- react-responsive-practice/src/App.js
+++ react-responsive-practice/src/App.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import useMediaQuery from 'react-responsive';
+import { useMediaQuery } from 'react-responsive';
 import './App.css';
 
-export default function App () {
-
-  const isTest = useMediaQuery ({
-    query : "(min-width : 800px)"
+// export default function App () {
+//   const isLarger = useMediaQuery ({
+//     query : '(min-width: 800px)'
+//   });
+//   const isSmaller = useMediaQuery ({
+//     query : '(max-width: 799px)'
+//   });
+//   console.log(isLarger);
+  
+//   return (
+//     <div>
+//       {isLarger && <p className='testText'>800px 이 넘었습니당.</p>}
+//       {isSmaller && <p>799px 이하입니다!</p>}
+//     </div>
+//   )
+// };
+const Example = () => {
+  const Large = ({children}) => {
+    const isLarge = useMediaQuery ({
+      query : '(min-width: 800px)'
+    })
+    return (isLarge ? children : null)
+  };
+  // const isLarger = useMediaQuery ({
+  //   query : '(min-width: 800px)'
+  // });
+  const isSmaller = useMediaQuery ({
+    query : '(max-width: 799px)'
   });
-  console.log(isTest);
+  console.log(isSmaller);
   
   return (
     <div>
-      {isTest && <p>This is Test</p>}
-      <p>기본 텍스트</p>
+      <Large>
+        800px이 넘었습니다.
+      </Large>
+      {/* {isLarger && <p className='testText'>800px 이 넘었습니당.</p>} */}
+      {isSmaller && <p>799px 이하입니다!</p>}
     </div>
   )
 };
+export default Example;
